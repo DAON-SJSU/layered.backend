59d300f8daf133d18bcaeb9e3628cb951c89e380
yuuki08noah <yuuki08noah@gmail.com>
2025-05-17 23:01:58 +0900

    feat :: model

---
+++ model.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from pydantic import BaseModel
+
+class Playlist(BaseModel):
+    emotion: str
+    genres: List[str] = None
+    tempo: float = 0.5
+    length: int = 20
+
+
+
